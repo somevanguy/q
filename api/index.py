1502a8ef38fc152963a94071dfc4c313a8753d0e
somevanguy <sbkereborn@gmail.com>
2025-02-17 20:29:55 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     if "WindowsPowerShell" in request.headers.get("User-Agent"):
-        return 'Set-Clipboard -Value "https://github.com/somevanguy/q/edit/main/api/index.py"; Write-Output "A little thing i made to quickly manage stuff, source code link copied to clipboard."', 200
+        return 'scb -Value "https://github.com/somevanguy/q/edit/main/api/index.py"; write "A little thing i made to quickly manage stuff, source code link copied to clipboard."', 200
     else:
         return 'A little thing i made to quickly manage stuff, source code: <a href="https://github.com/somevanguy/q/edit/main/api/index.py">https://github.com/somevanguy/q/edit/main/api/index.py</a>', 200
 
