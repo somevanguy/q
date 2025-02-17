cb700c85e8aee4a78a801c4b195dd575d83b1b09
somevanguy <sbkereborn@gmail.com>
2025-02-17 20:16:58 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+greetmsg = 'A little thing i made to quickly manage stuff, source code: "https://github.com/somevanguy/q/edit/main/api/index.py"'
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    if request.headers.get("User-Agent").find("WindowsPowerShell"):
+        return f'Write-Output "{greetmsg}"', 200
+    else:
+        return greetmsg, 200
 
-@app.route('/about')
+@app.route('/noblox')
 def about():
-    return 'About'
+    return 'temp'
